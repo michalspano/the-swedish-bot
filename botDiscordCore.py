d7edf5a4bd41afadad3f3ff829bc56868cc89822
michalspano <speko.spano@gmail.com>
2021-07-03 14:47:31 +0200

    03/07/2021 - new Google API module, fix, threading

--- botDiscordCore.py
+++ botDiscordCore.py
@@ -1,10 +1,10 @@
-import pandas as pd
-import random as r
 import json
 import discord
 import os
 import time
 
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 from flaskProvider import keep_alive
 
 client = discord.Client()
@@ -12,26 +12,20 @@ client = discord.Client()
 
 #  Class to process and embed a message
 class EmbedMessage:
-    def __init__(self, data, row_count, user):
-        self.data, self.row_count = data, row_count
+    def __init__(self, data, user, switch):
+        self.data = data
         self.user = user
+        self.switch = switch
 
     def embed_discord_message(self):
-        #  Chooses random row index
-        random_index = r.randint(0, self.row_count)
-
-        #  Transform all keys from the dict to a list
-        keys_list = self.data.keys()
-
-        #  Loads indexed data from dict to a list
-        random_data = [self.data[key][random_index] for key in keys_list]
-        embed_title = f"@{random_data[3]} tweeted at {random_data[1]} [{random_data[6]}]"
+        tweet_data = self.data
+        embed_title = f"@{tweet_data[3]} tweeted at {tweet_data[1]} [{tweet_data[6].upper()}]"
 
         #  Creates an embedded message with all settings respectively
-        embed_message = discord.Embed(title=embed_title, url=random_data[0],
-                                      description=random_data[2], colour=discord.Colour.blue())
-        embed_message.add_field(name="Number of likes", value=random_data[4], inline=True)
-        embed_message.add_field(name="Number of retweets", value=random_data[5], inline=True)
+        embed_message = discord.Embed(title=embed_title, url=tweet_data[0],
+                                      description=tweet_data[2], colour=discord.Colour.blue())
+        embed_message.add_field(name="Number of likes", value=tweet_data[4], inline=True)
+        embed_message.add_field(name="Number of retweets", value=tweet_data[5], inline=True)
         embed_message.set_footer(text=f"Requested by {self.user}")
 
         #  Returns an embedded discord object
@@ -45,7 +39,11 @@ class HelpCommands:
     #  Returns the --help command content with discord.Embed()
     def embed_help_commands(self):
         help_commands_desc = str()
+
+        #  Read from a local .txt with corresponding data
         help_data = [line for line in open(self.path, "r").readlines()]
+
+        #  Description data without last 3 lines (redundant \n, 2 hyperlink profiles)
         description_data = help_data[:len(help_data) - 3]
         for data_char in description_data:
             help_commands_desc += data_char
@@ -55,10 +53,14 @@ class HelpCommands:
                                       description=help_commands_desc,
                                       url=str(os.environ["GITHUB_LINK"]),
                                       color=discord.Color.from_rgb(255, 255, 0))
+
+        #  Additional fields with hyperlinked profiles
         embed_message.add_field(name="Twitter profile",
                                 value=str(help_data[-2]), inline=True)
         embed_message.add_field(name="Developed by",
                                 value=str(help_data[-1]), inline=True)
+
+        #  Author and icon set via client's cred. data
         embed_message.set_author(name=client.user.display_name,
                                  icon_url=client.user.avatar_url)
         return embed_message
@@ -66,14 +68,34 @@ class HelpCommands:
 
 #  Loads data from Google spreadsheet via Google API
 def load_secrets(path):
-    with open(path, ) as secretsJSON:  # Opens a json file with cred. details
+
+    #  Loads from .json module
+    with open(path, ) as secretsJSON:
         secrets = json.load(secretsJSON)
 
-    #  Processes the spreadsheet through .csv format and pandas onto a dataFrame
-    df = pd.read_csv(f"{secrets['provider']}{secrets['sheet_id']}/export?format=csv")
+    #  Service credentials configuration
+    SERVICE_ACCOUNT_FILE = secrets["creds"]
+    SCOPES = secrets["scope"]
 
-    #  Returns df in dict() form and the number of rows (index)
-    return [df.to_dict(), len(df.index) - 1]
+    #  API credentials with valid scope
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE,
+                                                                        scopes=SCOPES)
+
+    #  Spread sheet ID number
+    SAMPLE_SPREAD_SHEET_ID = secrets["sheet_id"]
+    service = build("sheets", "v4", credentials=credentials)
+    sheet = service.spreadsheets()
+
+    #  Resulting output with specified ID and range selection
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREAD_SHEET_ID,
+                                range="Sheet1!A:G").execute()
+
+    #  Creates a list instance from result scope
+    values = result.get("values", [])
+    last_index = len(values) - 1
+
+    #  Returns the latest thread
+    return values[last_index]
 
 
 @client.event
@@ -108,15 +130,13 @@ async def on_message(message, start_method="--"):
             embed_message = HelpCommands("help_command.txt").embed_help_commands()
             await message.channel.send(embed=embed_message)
 
-    # To do if --start was prompted by a user
-    while client_switch:
-        #  Loads the returned em. object from the custom class
-        obj_instance = load_secrets("secrets.json")
-        embed_object = EmbedMessage(obj_instance[0], obj_instance[1],
-                                    str(message.author)).embed_discord_message()
-        await message.channel.send(embed=embed_object)
-        #  Repeats periodically every hour
-        time.sleep(int(os.environ["TIME_INTERVAL"]))
+        while client_switch:
+            print("Tweeting...")
+            #  Loads the returned em. object from the custom class
+            embed_object = EmbedMessage(load_secrets("secrets.json"),
+                                        str(message.author), client_switch).embed_discord_message()
+            await message.channel.send(embed=embed_object)
+            time.sleep(3600)
 
 
 client_switch = False
